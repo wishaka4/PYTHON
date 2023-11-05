b77732e0f6b5116ebe3dc90f03f4efe2358f38d2
Vishakha <wishaka4@gmail.com>
2023-11-05 16:05:56 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+print("Hello");
+print("programming practise ")
+strng_name = "Vishakha Pratik Sutar"
+print(len(strng_name))
+
+mainstring = "Hello my world"
+print(mainstring)
+substring = "world"
+print(mainstring.find(substring))
+
+capitalstring = "VISHAKHA SUTAR"
+lowerstring= capitalstring.lower()
+print(lowerstring)
+
+firstlettercaptiral = capitalstring.capitalize()
+print(firstlettercaptiral)
+
+allfirstlettercaptial = mainstring.title()
+print(mainstring.title(allfirstlettercaptial))
